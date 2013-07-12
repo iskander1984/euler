8be2d5c26e2e1578f40efe333e57103521ba0b69
oleksandr <oleksandr.pochapskyy@skelia.com.ua>
2013-07-13 00:32:55 +0300

    Added second problem with unfold function as sequence generator

--- src/main/scala/org/euler/Problem1.scala
+++ src/main/scala/org/euler/Problem1.scala
@@ -1,6 +1,15 @@
 package org.euler
 
-object Problem1 extends Application {  
+/* 
+ * Even Fibonacci numbers
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * 
+ */
+
+object Problem1 extends App {  
   val is3Mult = (_: Int) % 3 == 0           
   val is5Mult = (_: Int) % 5 == 0         
 	
